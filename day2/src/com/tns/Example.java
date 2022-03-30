fcb1ed95c9762420fe124edb5745d8b73cef0c95
acer <acer@DESKTOP-GDIQML6>
2022-03-31 00:26:21 +0530

    first

---
+++ day2/src/com/tns/Example.java
@@ -0,0 +1,17 @@
+package com.tns;
+
+import java.util.Scanner;
+
+public class Example {
+
+	public static void main(String[] args) {
+		Scanner S=new Scanner(System.in);
+		String str=S.nextLine();
+		S.close();
+		
+		System.out.println(str);
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ day2/src/com/tns/Example2.java
@@ -0,0 +1,20 @@
+package com.tns;
+
+import java.util.Scanner;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		Scanner S=new Scanner(System.in);
+		int a=S.nextInt();
+		int b=S.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		S.close();
+		// TODO Auto-generated method stub
+
+	}
+
+}
