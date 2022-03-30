852ea10c6056c63a0cd19314b639079684b785e8
acer <acer@DESKTOP-GDIQML6>
2022-03-30 13:13:00 +0530

    day1

---
+++ B1_4 corejava/src/day1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class day1 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to java projects");
+		// TODO Auto-generated method stub
+
+	}
+
+}
